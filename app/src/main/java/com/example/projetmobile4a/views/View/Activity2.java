354e5a78f7c5e89d86ef25c487a3e78a606f84f3
emmaS <siggini@et.esiea.fr>
2019-12-21 23:48:21 +0100

    Add second activity

--- app/src/main/java/com/example/projetmobile4a/views/View/Activity2.java
+++ app/src/main/java/com/example/projetmobile4a/views/View/Activity2.java
@@ -13,7 +13,7 @@ public class Activity2 extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_2);
+    setContentView(R.layout.activity2);
 
   }
 }
